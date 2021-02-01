96d240e3c9ca0d21fec1c86a181a5f89da7770d5
WuChungYin <32268883+WuChungYin@users.noreply.github.com>
2021-02-01 14:46:37 -0600

    Create mathFunction.js

---
+++ mathFunction.js
@@ -0,0 +1,33 @@
+function mathFunction(a, b, c){
+	var none = true;
+  var result = "";
+  
+  if(a + b == c){
+  	none = false;
+    result += '+';
+  }
+    if(a - b == c){
+  	none = false;
+    result += '-';
+  }
+    if(a * b == c){
+  	none = false;
+    result += '*';
+  }
+    if(a / b == c){
+  	none = false;
+    result += '/';
+  }
+  if(none == true){
+  	result = 'None';
+  }
+  
+  console.log(result);
+}
+
+mathFunction(1,2,3);
+mathFunction(2,2,4);
+mathFunction(3,-3,-9);
+mathFunction(1,2,-1);
+mathFunction(3,3,1);
+mathFunction(7,1,11);
