c3571e2829934ed6f57109ef65059cb35e11428e
WuChungYin <32268883+WuChungYin@users.noreply.github.com>
2021-02-01 15:04:21 -0600

    Update mathFunction.js

--- mathFunction.js
+++ mathFunction.js
@@ -1,33 +1,38 @@
 function mathFunction(a, b, c) {
-    var none = true;
-    var result = "";
+    if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c)) {
+        var none = true;
+        var result = "";
 
-    if (a + b == c) {
-        none = false;
-        result += '+';
-    }
-    if (a - b == c) {
-        none = false;
-        result += '-';
-    }
-    if (a * b == c) {
-        none = false;
-        result += '*';
-    }
-    if (a / b == c) {
-        none = false;
-        result += '/';
-    }
-    if (none == true) {
-        result = 'None';
-    }
+        if (a + b == c) {
+            none = false;
+            result += '+';
+        }
+        if (a - b == c) {
+            none = false;
+            result += '-';
+        }
+        if (a * b == c) {
+            none = false;
+            result += '*';
+        }
+        if (a / b == c) {
+            none = false;
+            result += '/';
+        }
+        if (none == true) {
+            result = 'None';
+        }
 
-    console.log(result);
+        console.log(result);
+    } else {
+        console.log('Please make sure your inputs are integers.')
+    }
 }
 
-mathFunction(1, 2, 3);
-mathFunction(2, 2, 4);
-mathFunction(3, -3, -9);
-mathFunction(1, 2, -1);
-mathFunction(3, 3, 1);
-mathFunction(7, 1, 11);
+mathFunction(1, 2, 3); // '+'
+mathFunction(2, 2, 4); // '+*'
+mathFunction(3, -3, -9); // '*'
+mathFunction(1, 2, -1); // '-'
+mathFunction(3, 3, 1); // '/'
+mathFunction(7, 1, 11); // 'None'
+mathFunction('a', 'b', 'c'); // 'Please make sure your inputs are integers.'
